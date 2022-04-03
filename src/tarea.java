74ae3d0362bbf1c1a1d8679b07c30a70389bdc1f
Jorge Romero <jorge.romero.romero@gmail.com>
2022-04-02 23:32:23 -0300

    Add files via upload

---
+++ src/tarea.java
@@ -0,0 +1,10 @@
+con = pool.getConnection();
+String sql = "select * from user where username ='"+ username +"' and password = '" + password + "'";
+stmt = conn.createStatement();
+rs = stmt.executeQuery(sql);
+if (rs.next()){
+	loggedIn = true;
+	out.println(Autenticado exitosamente);
+} else {
+	out.println("usuario y/o password noreconocida");
+}
